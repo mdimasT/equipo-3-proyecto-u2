9bf61ba6b578c901ecc8a930236e952f365acb2b
Victor Carmona <victor181851@gmail.com>
2023-07-06 20:39:51 -0600

    feat: Initialize data structures and main menu
    Import stack, linked_list, and ds_queue modules and initialize the data structures. Add main_menu function with menu options and logic to control program flow.

---
+++ main.py
@@ -0,0 +1,34 @@
+import stack, linked_list, ds_queue
+menu_control = True
+def main_menu():
+    global menu_control
+    
+    print("Menu")
+    print("")
+    print("Choose an option")
+    print("1.-Queue")
+    print("2.-Stack")
+    print("3.-Linked list")
+    print("4.-Exit")
+ 
+    option = input()
+    if option.upper() == "QUEUE" or option == "1":
+        queue_menu()
+
+    elif option.upper() == "STACK" or option == "2":
+        stack_menu()
+
+    elif option.upper() == "LINKED LIST" or option == "3":
+        linked_list_menu()
+
+    elif option.upper() == "EXIT" or option == "4":
+        menu_control = False
+
+myqueue = ds_queue.Queue()
+mystack = stack.Stack()
+myLinkedList = linked_list.LinkedList()
+
+    
+
+
+ 
